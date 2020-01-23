a6f4a6b16a95e9e118bf1f2e4870626a7f7e02d8
Eduardo Tenório <embatbr@gmail.com>
2020-01-23 17:39:58 -0300

    Version 0.7.0 with command 'bigquery create views' renamed to 'bigquery create mirror-views', with flag for prefix 'VW_', and new command 'bigquery create views' using SQL scripts to create more complex views.

--- enola/products/bigquery.py
+++ enola/products/bigquery.py
@@ -58,20 +58,46 @@ def _create_table(project, dataset_name, name, schemaspath):
     run_cmd(_TEMPLATE, template_args)
 
 
-def _create_views(views_project, views_dataset, origin_project, origin_dataset, table):
+def _create_mirror_view(views_project, views_dataset, origin_project, origin_dataset,
+    table, view):
     _TEMPLATE = [
         "bq --project_id {views_project} mk",
         "--use_legacy_sql=false",
         "--view 'SELECT * FROM `{origin_project}.{origin_dataset}.{table}`'",
-        "{views_dataset}.{table}"
+        "{views_dataset}.{view}"
     ]
+
+    if not view:
+        view = table
 
     template_args = {
         'views_project': views_project,
         'views_dataset': views_dataset,
         'origin_project': origin_project,
         'origin_dataset': origin_dataset,
-        'table': table
+        'table': table,
+        'view': view
+    }
+
+    run_cmd(_TEMPLATE, template_args)
+
+
+def _create_view(views_project, views_dataset, table, view_sql_path):
+    _TEMPLATE = [
+        "bq --project_id {views_project} mk",
+        "--use_legacy_sql=false",
+        "--view '{view_sql}'",
+        "{views_dataset}.{table}"
+    ]
+
+    with open(view_sql_path) as f:
+        view_sql = f.read()
+
+    template_args = {
+        'views_project': views_project,
+        'views_dataset': views_dataset,
+        'table': table,
+        'view_sql': view_sql
     }
 
     run_cmd(_TEMPLATE, template_args)
@@ -135,7 +161,8 @@ def table(project, dataset_name, name, schemaspath):
 @click.argument('views_dataset')
 @click.argument('origin_project')
 @click.argument('origin_dataset')
-def views(views_project, views_dataset, origin_project, origin_dataset):
+@click.option('--vw-prefix', 'vw_prefix', is_flag=True)
+def mirror_views(views_project, views_dataset, origin_project, origin_dataset, vw_prefix):
     _create_dataset(views_project, views_dataset)
 
     cmd_default_context = get_default_context()
@@ -144,15 +171,42 @@ def views(views_project, views_dataset, origin_project, origin_dataset):
 
     filenames = os.listdir(schemaspath)
     tables = [filename[0 : -5] for filename in filenames]
+    views = list()
     for table in tables:
-        _create_views(views_project, views_dataset, origin_project, origin_dataset, table)
+        view = 'VW_{}'.format(table) if vw_prefix else table
+        views.append(view)
+
+        _create_mirror_view(views_project, views_dataset, origin_project, origin_dataset,
+            table, view)
+
+    _share_views(views_project, views_dataset, origin_project, origin_dataset, views)
+
+
+@click.command()
+@click.argument('views_project')
+@click.argument('views_dataset')
+@click.argument('origin_project')
+@click.argument('origin_dataset')
+def views(views_project, views_dataset, origin_project, origin_dataset):
+    _create_dataset(views_project, views_dataset)
+
+    cmd_default_context = get_default_context()
+    gcp_path = '{}/gcp'.format(cmd_default_context['cwd'])
+    queriespath = '{}/bigquery/queries/{}'.format(gcp_path, views_dataset)
+
+    filenames = os.listdir(queriespath)
+    tables = [filename[0 : -4] for filename in filenames]
+    for table in tables:
+        view_sql_path = '{}/{}.sql'.format(queriespath, table)
+        _create_view(views_project, views_dataset, table, view_sql_path)
 
     _share_views(views_project, views_dataset, origin_project, origin_dataset, tables)
 
 
 create.add_command(dataset)
 create.add_command(table)
-create.add_command(views)
+create.add_command(mirror_views, 'mirror-views')
+create.add_command(views, 'views')
 
 
 # GROUP create END
